c8abf2ddc19960c4f212af630cba179eba6f2b41
GVRs96 <info.govorov@gmail.com>
2026-05-01 14:52:12 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // ЗАМЕНИ "your-repo-name" на название своего GitHub репозитория!
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/',
+  base: '/GVR-APP/',
 })
